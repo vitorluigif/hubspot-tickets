9f87f131409b8b0c04472a2ed8996f331874831a
vitorluigif <vitorluigitrabalho@gmail.com>
2026-05-12 15:29:22 -0300

    fix: remove cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,15 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://hubspot-tickets.vercel.app",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get("/health", (_: Request, res: Response) => {
